26dd81e06cd4b30ba9197797279ec6814db4785a
mingyeong <mingang0830@gmail.com>
2022-03-26 16:22:56 +0900

    Leet Code
    Implement strStr() 풀이

---
+++ 202203/implement_str.py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+
+        if needle in haystack:
+            return len(haystack.split(needle)[0])
+        return -1
+
+
