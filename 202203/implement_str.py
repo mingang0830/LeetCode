c0cccd43b4ea2d591c1e6b598081a0675efe6a84
mingyeong <mingang0830@gmail.com>
2022-03-27 12:57:26 +0900

    Leet Code
    Implement strStr() 수정

--- 202203/implement_str.py
+++ 202203/implement_str.py
@@ -3,8 +3,19 @@ class Solution:
         if len(needle) == 0:
             return 0
 
+        '''
         if needle in haystack:
             return len(haystack.split(needle)[0])
+        '''
+
+        if needle in haystack:
+            div = len(needle)
+            for idx in range(0, len(haystack)):
+                temp = haystack[idx:div]
+                if temp == needle:
+                    return idx
+                div += 1
         return -1
+
 
 
