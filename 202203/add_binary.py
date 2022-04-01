f777a9868f35a7e96f0782882a066360ef1ac733
mingyeong <mingang0830@gmail.com>
2022-04-01 17:15:05 +0900

    Leet Code 코드 수정
    [::-1] 제거

--- 202203/add_binary.py
+++ 202203/add_binary.py
@@ -1,19 +1,19 @@
 class Solution:
-    def decimal(self, bin_: int) -> int:
+    def decimal(self, bin_: str) -> int:
         result: int = 0
-        bin_temp: str = str(bin_)[::-1]
-        for i in range(len(bin_temp[::-1])):
-            if bin_temp[i] == "0":
+        i: int = 0
+        for j in range(len(bin_)-1, -1, -1):
+            if bin_[j] == "0":
                 pass
-            elif bin_temp[i] == "1":
+            elif bin_[j] == "1":
                 result += 2 ** i
-
+            i += 1
         return result
 
     def addBinary(self, a: str, b: str) -> str:
         # return format(int(a, 2) + int(b, 2), 'b')
-        return format(self.decimal(int(a)) + self.decimal(int(b)), 'b')
+        return format(self.decimal(a) + self.decimal(b), 'b')
 
 
-
+print((Solution().addBinary("1010", "1011")))
 
