95e6ad314d18bde7bfe81397791e244be9b6c104
mingyeong <mingang0830@gmail.com>
2022-05-17 23:07:27 +0900

    leet code
    add digits 풀이
    재귀 함수 사용해서 풀었다
    temp_num 에 자릿수들의 값을 더함
    숫자를 줄이기 위해 num//10
    num이 한자릿수가 될때까지 재귀

---
+++ 202205/add_digits.py
@@ -0,0 +1,17 @@
+from curses.ascii import SO
+
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if(num < 10):
+            return num
+        
+        temp_num: int = 0
+        while(num > 0):
+            temp_num += num%10
+            num = num//10            
+            
+        return self.addDigits(temp_num)
+
+if __name__ == "__main__":
+    print(Solution().addDigits(38))
