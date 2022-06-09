d4e608b32913ac0fef0c402e1d8c091a1b53d0ed
mingyeong <mingang0830@gmail.com>
2022-06-09 21:30:21 +0900

    leetcode
    container with most water 수정
    정답은 two pointer였다!
    discuss에서 힌트를 얻으려고 보는데 python two pointer 제목을 보게돼서 양 끝을 pointer로 두고 짜게 됐다
    효율성을 높이는 방법에는 hash map, binary search, two pointer 이렇게 세가지 방법이 많이 쓰이는것 같다

--- 202206/container_with_most_water.py
+++ 202206/container_with_most_water.py
@@ -11,5 +11,16 @@ class Solution:
                 max_area = max(max_area,min(height[i],height[j])*(j-i))
         return max_area
         """
+        left=0
+        right=len(height)-1
+        max_area=0
 
-        
+        while (right-left>0) :
+            max_area = max(max_area,(right-left)*min(height[left],height[right]))
+                
+            if height[left] >= height[right]: # 오른쪽이 낮은 경우
+                right -= 1
+            else: # 왼쪽이 낮은 경우
+                left += 1
+            
+        return max_area
